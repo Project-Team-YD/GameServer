ed55fa0a6fba328dd91f4cb6f16245372ca2d96e
jaycci <pm.jaycci@gmail.com>
2023-09-27 09:44:25 +0900

    프로토파일 수정 및 grpc 래핑

--- main.go
+++ main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"context"
-	"net"
-	global_grpc "project_yd/grpc"
-
-	"google.golang.org/grpc"
+	server "project_yd/server"
 	//itemcreateevent "github.com/heroiclabs/nakama/v3/nurhyme_common/ItemCreateEvent"
 )
 
-type server struct {
-	global_grpc.UnimplementedGlobalGRpcServiceServer
-}
-
-func (s *server) SayHello(ctx context.Context, in *global_grpc.GlobalGrpcRequest) (*global_grpc.GlobalGrpcResponse, error) {
-	println("Request!! Rpc Key::", in.RpcKey, "/ Message::", in.Message)
-	result := &global_grpc.GlobalGrpcResponse{}
-
-	result.Message = "Test Response Success"
-	return result, nil
-}
-
 func main() {
-	lis, err := net.Listen("tcp", ":19001")
-	if err != nil {
-		println(err)
-	}
-
-	grpcServer := grpc.NewServer()
-	global_grpc.RegisterGlobalGRpcServiceServer(grpcServer, &server{})
-
-	println("server listening at ", lis.Addr())
-
-	if err := grpcServer.Serve(lis); err != nil {
-		println("grpcServer Serv Err!!::", err)
-	}
+	//-- start grpc server
+	server.StartGrpcServer()
 }
--- server/global_grpc_manager.go
+++ server/global_grpc_manager.go
@@ -4,23 +4,53 @@ import (
 	"context"
 	"net"
 	global_grpc "project_yd/grpc"
+	"sync"
 
 	"google.golang.org/grpc"
 )
+
+var GlobalGrpcEvent *sync.Map
+
+// 함수 시그니처: 클라이언트로부터 받은 key 값에 따라 호출할 함수들
+type RpcKeyHandlerFunc func(payload string) string
+
+//-- rpc로 쓰일 function 등록
+func RegistRpc(rpcKey string, function RpcKeyHandlerFunc) {
+	_, ok := GlobalGrpcEvent.Load(rpcKey)
+	if ok {
+		println("RPC KEY : ", rpcKey, " is Duplicate!")
+		return 	
+	}
+	GlobalGrpcEvent.Store(rpcKey, function)
+	println("RPC KEY : ", rpcKey, " is Regist Success!")
+}
+
+//-- 클라이언트로부터 호출된 rpc명으로 호출할 function을 찾고 payload값을 넘겨주어 결과값을 받는다.
+func LoadRpc(rpcKey string, payload string) string {
+	var result string
+	rpcFunc, ok := GlobalGrpcEvent.Load(rpcKey)
+	if !ok {
+		result = "Not Found Rpc Key :" + rpcKey
+		println(result)
+		return result
+	} else {
+		if function, ok := rpcFunc.(RpcKeyHandlerFunc); ok {
+			return function(payload)
+		} else {
+			result = "Result is not RpcKeyHandlerFunc Type"
+			return result
+		}
+	}
+}
 
 type GrpcServer struct {
 	global_grpc.UnimplementedGlobalGRpcServiceServer
 }
 
-type GrpcMessage struct {
-	RequestMessage *global_grpc.GlobalGrpcRequest
-}
-
-func (server *GrpcServer) ListenGrpc(ctx context.Context, request *global_grpc.GlobalGrpcRequest) (*global_grpc.GlobalGrpcResponse, error) {
-	message := GrpcMessage{}
-	message.RequestMessage = request
-
+func (server *GrpcServer) GlobalGRpc(ctx context.Context, request *global_grpc.GlobalGrpcRequest) (*global_grpc.GlobalGrpcResponse, error) {
 	result := &global_grpc.GlobalGrpcResponse{}
+	result.Message = LoadRpc(request.RpcKey, request.Message)
+
 	return result, nil
 }
 
@@ -31,6 +61,8 @@ func GrpcServe(grpcServer *grpc.Server, listen net.Listener) {
 }
 
 func StartGrpcServer() {
+	GlobalGrpcEvent = new(sync.Map)
+
 	grpcServer := grpc.NewServer()
 	lis, err := net.Listen("tcp", SERVER_PORT)
 	if err != nil {
